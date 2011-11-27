c94614d364a911b292e846733221f1bf02f8ba9e
badgerboy132@gmail.com <badgerboy132@gmail.com@dcbf4249-b2f0-0c42-cf25-d37c1ec6a844>
2011-11-27 23:48:46 +0000

    update bench

--- ispass2009-benchmarks/benchmarks/CUDA/STO/buildbenchmark.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#/home/sarah/cuda/bin/nvcc rayTracing.cu -I"/home/sarah/NVIDIA_GPU_Computing_SDK/C/common/inc/" EasyBMP.cpp makebmp.cpp /home/sarah/NVIDIA_GPU_Computing_SDK/C/lib/libcutil_x86_64.a
-NVCC=/home/sarah/cuda/bin/nvcc
-INCLUDE=/home/sarah/NVIDIA_GPU_Computing_SDK/C/common/inc/
-LINK=/home/sarah/NVIDIA_GPU_Computing_SDK/C/lib/libcutil_x86_64.a 
-$NVCC md5_kernel.cu sha1_kernel.cu main.cu -I$INCLUDE $LINK
-#$NVCC md5_kernel.cu sha1_kernel.cu storeGPU.cu main.cu -I$INCLUDE storeCPU.c md5_cpu.c sha1_cpu.c $LINK
